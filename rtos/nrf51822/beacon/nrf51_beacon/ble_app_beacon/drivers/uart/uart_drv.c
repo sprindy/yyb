cbf39efdd7e0c029570993c3e55e13daf043a837
sprindy <sprindy.liu@gmail.com>
2017-05-15 20:15:55 +0800

    [YYB][RTOS] Enable printf.
    Symptom:
    Root Cause: N/A
    Solution: N/A
    Project: YYB.
    Note: N/A
    Test done by RD
       -Build pass: Y
       -Feature verification: Y

--- rtos/nrf51822/beacon/nrf51_beacon/ble_app_beacon/drivers/uart/uart_drv.c
+++ rtos/nrf51822/beacon/nrf51_beacon/ble_app_beacon/drivers/uart/uart_drv.c
@@ -1,119 +1,51 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include "pca20006.h"
-/* #include "app_uart.h" */
+#include "app_uart.h"
 #include "uart_drv.h"
 
 #include "nrf.h"
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
 
-uint8_t uart_get(void)
+#define UART_TX_BUF_SIZE 256                         /**< UART TX buffer size. */
+#define UART_RX_BUF_SIZE 256                         /**< UART RX buffer size. */
+
+static void uart_error_handler(app_uart_evt_t * p_event)
 {
-    while (NRF_UART0->EVENTS_RXDRDY != 1)
-    {
-        // Wait for RXD data to be received
-    }
-
-    NRF_UART0->EVENTS_RXDRDY = 0;
-    return (uint8_t)NRF_UART0->RXD;
-}
-
-
-bool uart_get_with_timeout(int32_t timeout_ms, uint8_t * rx_data)
-{
-    bool ret = true;
-
-    while (NRF_UART0->EVENTS_RXDRDY != 1)
-    {
-        if (timeout_ms-- >= 0)
-        {
-            // wait in 1ms chunk before checking for status.
-            nrf_delay_us(1000);
-        }
-        else
-        {
-            ret = false;
-            break;
-        }
-    } // Wait for RXD data to be received.
-
-    if (timeout_ms >= 0)
-    {
-        // clear the event and set rx_data with received byte.
-        NRF_UART0->EVENTS_RXDRDY = 0;
-        *rx_data                 = (uint8_t)NRF_UART0->RXD;
-    }
-
-    return ret;
-}
-
-void uart_put(uint8_t cr)
-{
-    NRF_UART0->TXD = (uint8_t)cr;
-
-    while (NRF_UART0->EVENTS_TXDRDY != 1)
-    {
-        // Wait for TXD data to be sent.
-    }
-
-    NRF_UART0->EVENTS_TXDRDY = 0;
-}
-
-
-void uart_putstring(const uint8_t * str)
-{
-    uint_fast8_t i  = 0;
-    uint8_t      ch = str[i++];
-
-    while (ch != '\0')
-    {
-        uart_put(ch);
-        ch = str[i++];
+    switch (p_event->evt_type) {
+		case APP_UART_COMMUNICATION_ERROR:
+	        APP_ERROR_HANDLER(p_event->data.error_communication);
+	    break;
+	    case APP_UART_FIFO_ERROR:
+	    	APP_ERROR_HANDLER(p_event->data.error_code);
+	    break;
+        default: break;
     }
 }
 
 uint32_t uart_init(void)
 {
 	uint32_t err_code;
-#if 0
+
 	app_uart_comm_params_t common_params = {
 		RX_PIN_NUMBER,
 		TX_PIN_NUMBER,
 		RTS_PIN_NUMBER,
 		CTS_PIN_NUMBER,
-		(app_uart_flow_control_t) APP_UART_FLOW_CONTROL_DISABLED,
+		APP_UART_FLOW_CONTROL_DISABLED,
 		HWFC,
-		UART_BAUDRATE
+		UART_BAUDRATE_BAUDRATE_Baud38400
 	};
 
-	err_code = app_uart_init(&common_params,
-			NULL,
-			NULL,
+	/* err_code = app_uart_init(&common_params, */
+	APP_UART_FIFO_INIT(&common_params,
+			UART_RX_BUF_SIZE,
+			UART_TX_BUF_SIZE,
+			uart_error_handler,
 			APP_IRQ_PRIORITY_LOW,
-			0);
-#endif
-
-/** @snippet [Configure UART RX and TX pin] */
-    nrf_gpio_cfg_output(TX_PIN_NUMBER);
-    nrf_gpio_cfg_input(RX_PIN_NUMBER, NRF_GPIO_PIN_NOPULL);
-
-    NRF_UART0->PSELTXD = TX_PIN_NUMBER;
-    NRF_UART0->PSELRXD = RX_PIN_NUMBER;
-/** @snippet [Configure UART RX and TX pin] */
-    if (HWFC) {
-        nrf_gpio_cfg_output(RTS_PIN_NUMBER);
-        nrf_gpio_cfg_input(CTS_PIN_NUMBER, NRF_GPIO_PIN_NOPULL);
-        NRF_UART0->PSELCTS = CTS_PIN_NUMBER;
-        NRF_UART0->PSELRTS = RTS_PIN_NUMBER;
-        NRF_UART0->CONFIG  = (UART_CONFIG_HWFC_Enabled << UART_CONFIG_HWFC_Pos);
-    }
-
-    NRF_UART0->BAUDRATE      = (UART_BAUDRATE_BAUDRATE_Baud38400 << UART_BAUDRATE_BAUDRATE_Pos);
-    NRF_UART0->ENABLE        = (UART_ENABLE_ENABLE_Enabled << UART_ENABLE_ENABLE_Pos);
-    NRF_UART0->TASKS_STARTTX = 1;
-    NRF_UART0->TASKS_STARTRX = 1;
-    NRF_UART0->EVENTS_RXDRDY = 0;
+			err_code);
+	APP_ERROR_CHECK(err_code);
 
 	return err_code;
 }
