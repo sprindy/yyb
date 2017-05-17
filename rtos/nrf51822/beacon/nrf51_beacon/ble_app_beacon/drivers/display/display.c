85b13a647c4d4b3ff8f93516c230f5553736c2e0
sprindy <sprindy.liu@gmail.com>
2017-05-18 01:39:34 +0800

    [YYB][RTOS] From line to plane.
    Symptom:
    Root Cause: N/A
    Solution: N/A
    Project: YYB.
    Note: Need to optimize delay time.
    Test done by RD
       -Build pass: Y
       -Feature verification: Y

--- rtos/nrf51822/beacon/nrf51_beacon/ble_app_beacon/drivers/display/display.c
+++ rtos/nrf51822/beacon/nrf51_beacon/ble_app_beacon/drivers/display/display.c
@@ -14,12 +14,35 @@ typedef struct
 }display_context_t;
 
 static display_context_t m_display_ct = {0};
+char wData[128] = {
+	0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x08,0x00,0x00,0x00,0x08,
+	0x02,0x00,0x00,0x08,0x02,0x00,0x00,0x08,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,
+	0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,
+	0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,0x03,0xFF,0xFF,0xF8,
+	0x03,0xFF,0xFF,0xF8,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,
+	0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,0x02,0x00,0x40,0x08,
+	0x02,0x00,0x80,0x08,0x06,0x00,0x80,0x08,0x0E,0x00,0x00,0x08,0x06,0x00,0x00,0x18,
+	0x02,0x00,0x00,0x18,0x00,0x00,0x00,0x08,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
+    };
 
 /* turn on selected led on, and the others will off */
-static void display_led_on(uint8_t index, bool on)
+static void display_turn_led_on(uint8_t index, bool on)
 {
 	if( index >= DISPLAY_LED_NUM)
 		return;
+
+	if(on == false) {
+		nrf_gpio_cfg_input(DISPLAY_GPIO_0, NRF_GPIO_PIN_NOPULL);
+		nrf_gpio_cfg_input(DISPLAY_GPIO_1, NRF_GPIO_PIN_NOPULL);
+		nrf_gpio_cfg_input(DISPLAY_GPIO_2, NRF_GPIO_PIN_NOPULL);
+		nrf_gpio_cfg_input(DISPLAY_GPIO_3, NRF_GPIO_PIN_NOPULL);
+		nrf_gpio_cfg_input(DISPLAY_GPIO_4, NRF_GPIO_PIN_NOPULL);
+		nrf_gpio_cfg_input(DISPLAY_GPIO_5, NRF_GPIO_PIN_NOPULL);
+		nrf_gpio_cfg_input(DISPLAY_GPIO_6, NRF_GPIO_PIN_NOPULL);
+		nrf_gpio_cfg_input(DISPLAY_GPIO_7, NRF_GPIO_PIN_NOPULL);
+
+		return;
+	}
 
 	/* reinit all led gpio as output */
 	nrf_gpio_cfg_output(DISPLAY_GPIO_0);
@@ -357,13 +380,26 @@ static void display_led_on(uint8_t index, bool on)
 	}
 }
 
+void display_one_line(uint32_t line_data)
+{
+	static uint32_t tmp = 0;
+	static uint8_t i = DISPLAY_LED_NUM - 1;
+	/* for(uint8_t i=0; i<DISPLAY_LED_NUM, i++) */
+	{
+		tmp = line_data & (1 << (i));
+		display_turn_led_on(i, (tmp != 0));
+	}
+	if(i-- == 0)
+		i = DISPLAY_LED_NUM - 1;
+}
+
 uint32_t display_timer_start(void)
 {
 #if 1
     if (!m_display_ct.timer_running) {
         uint32_t err_code;
 
-        err_code = app_timer_start(m_display_ct.timer_id, 0x1000, NULL);
+        err_code = app_timer_start(m_display_ct.timer_id, 0x10, NULL);
         if (err_code != NRF_SUCCESS) {
 			printf("display timer start fail\n");
             return err_code;
@@ -378,15 +414,32 @@ uint32_t display_timer_start(void)
 
 static void display_work_timerout(void *p_context)
 {
-	uint32_t err_code;
+#define DISPLAY_LINE_DELAY  20
 	static uint8_t i = 0;
+	static uint8_t j = 0;
 
-	printf("%s i:%d\n",__func__, i);
-	display_led_on(i, true);
-	if(++i >= DISPLAY_LED_NUM)
+	static uint32_t cnt_line = 1;
+	static uint32_t tmp = 0x0;
+	if (i <= DISPLAY_LED_NUM - 1) {
+		display_one_line(tmp);
+		/* display_turn_led_on(i++, true); */
+	}
+
+	if(cnt_line%(DISPLAY_LED_NUM * DISPLAY_LINE_DELAY) == 0) {
+		printf("i:%d j:%d cnt_line:%d, tmp:0x%x\n", i, j, cnt_line, tmp);
 		i = 0;
-	/* err_code = app_timer_start(m_display_ct.timer_id, 1000, NULL); */
-	/* APP_ERROR_CHECK(err_code); */
+		cnt_line = 0;
+		tmp = 0;
+		tmp |= wData[j*4]<<24;
+	    tmp |= wData[j*4+1]<<16;
+	    tmp |= wData[j*4+2]<<8;
+	    tmp |= wData[j*4+3];
+		if(++j == sizeof(wData)/4) {
+			j = 0;
+		}
+	}
+
+	cnt_line++;
 }
 
 uint32_t display_work_init(void)
