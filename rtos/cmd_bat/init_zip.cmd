b5a8813873bebeb699522f31fc811dcad029af47
sprindy <sprindy.liu@gmail.com>
2017-06-06 01:05:17 +0800

    [YYB][RTOS][TOOL] Add batch script to replace keil.
    Symptom:
    Root Cause: N/A
    Solution: N/A
    Project: YYB.
    Note: N/A
    Test done by RD
       -Build pass: Y
       -Feature verification: Y

--- rtos/cmd_bat/init_zip.cmd
+++ rtos/cmd_bat/init_zip.cmd
@@ -1,20 +1,48 @@
 @echo off
 set fromelfPath=C:\Keil_v5\ARM\ARMCC\bin
 set nrfutilPath="C:\Program Files\Nordic Semiconductor\Master Control Panel\3.10.0.14\nrf"
-::set outputPath="X:\vr\YYB\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
-::set outputPath="Y:\yyb\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
-::set outputPath="Y:\yyb\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
-set outputPath="Y:\yyb\rtos\nrf51822\beacon\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
-::set outputPath="Y:\yyb\rtos\nrf51822\beacon\nrf51_beacon\ble_app_beacon_dfu\bootloader\pca20006\dual_bank_ble_s110\arm\_build"
 
-set outputFileName=nrf51822_beacon_app
+::set projectRootDir="X:\vr\YYB\rtos\nrf51822\beacon\"
+::set projectRootDir="Y:\yyb\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\"
+set projectRootDir=Y:\yyb\rtos\nrf51822\beacon\
 
+IF %1==app (
+	::default update applicaton
+	set outputFileName=nrf51822_beacon_app
+	set outputPath=%projectRootDir%nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build
+	set CUR_DIR=%~dp0
+	cd %projectRootDir%nrf51_beacon\ble_app_beacon\pca20006\s110\arm\
+	call nrf51822_xxac_s110.BAT
+	::cd CUR_DIR
+)
+IF %1==bl (
+	set outputFileName=nrf51822_beacon_bootloader
+	set outputPath=%projectRootDir%nrf51_beacon\ble_app_beacon_dfu\bootloader\pca20006\dual_bank_ble_s110\arm\_build
+)
 set axfFileName=%outputFileName%.axf
 set binFileName=%outputFileName%.bin
 set zipFileName=%outputFileName%.zip
 
-::%fromelfPath%\fromelf.exe --bin --output %outputPath%\%binFileName% %outputPath%\%axfFileName%
 
-%nrfutilPath%\nrfutil.exe dfu genpkg --application %outputPath%\%binFileName% --application-version 0xff %outputPath%\%zipFileName%
+::echo execute file : %0
+::echo first parameter : %1
+echo %outputPath%\%zipFileName%
+echo %outputPath%\%binFileName%
+
+IF %1==bl (
+	echo update bootloader
+	%fromelfPath%\fromelf.exe --bin --output %outputPath%\%binFileName% %outputPath%\%axfFileName%
+	%nrfutilPath%\nrfutil.exe dfu genpkg --bootloader %outputPath%\%binFileName% --dev-type 0xffff %outputPath%\%zipFileName%
+	adb.exe push %outputPath%\%zipFileName% \sdcard\Download
+)
+
+IF %1==app (
+	echo update app
+	%nrfutilPath%\nrfutil.exe dfu genpkg --application %outputPath%\%binFileName% --application-version 0xff %outputPath%\%zipFileName%
+)
 
 adb.exe push %outputPath%\%zipFileName% \sdcard\Download
+
+::fromelf --bin -o _build\nrf51822_beacon_app.bin _build\nrf51822_beacon_app.axf
+::fromelf --bin -o _build\nrf51822_beacon_bootloader.bin _build\nrf51822_beacon_bootloader.axf
+														
