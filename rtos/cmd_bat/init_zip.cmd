2f9dad9e50b50a3ca8edc3d593ee5af1d9b1b21f
sprindy <sprindy.liu@gmail.com>
2017-06-05 18:11:47 +0800

    [YYB][TOOL] Add adb and modify bat script.

--- rtos/cmd_bat/init_zip.cmd
+++ rtos/cmd_bat/init_zip.cmd
@@ -1,7 +1,11 @@
 @echo off
 set fromelfPath=C:\Keil_v5\ARM\ARMCC\bin
 set nrfutilPath="C:\Program Files\Nordic Semiconductor\Master Control Panel\3.10.0.14\nrf"
-set outputPath="X:\vr\YYB\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
+::set outputPath="X:\vr\YYB\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
+::set outputPath="Y:\yyb\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
+::set outputPath="Y:\yyb\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
+set outputPath="Y:\yyb\rtos\nrf51822\beacon\nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build"
+::set outputPath="Y:\yyb\rtos\nrf51822\beacon\nrf51_beacon\ble_app_beacon_dfu\bootloader\pca20006\dual_bank_ble_s110\arm\_build"
 
 set outputFileName=nrf51822_beacon_app
 
@@ -9,6 +13,8 @@ set axfFileName=%outputFileName%.axf
 set binFileName=%outputFileName%.bin
 set zipFileName=%outputFileName%.zip
 
-%fromelfPath%\fromelf.exe --bin --output %outputPath%\%binFileName% %outputPath%\%axfFileName%
+::%fromelfPath%\fromelf.exe --bin --output %outputPath%\%binFileName% %outputPath%\%axfFileName%
 
 %nrfutilPath%\nrfutil.exe dfu genpkg --application %outputPath%\%binFileName% --application-version 0xff %outputPath%\%zipFileName%
+
+adb.exe push %outputPath%\%zipFileName% \sdcard\Download
