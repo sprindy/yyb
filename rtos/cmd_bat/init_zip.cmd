1132b76f9b96a812754a01d2b9051a541ff036a3
sprindy <sprindy.liu@gmail.com>
2017-06-06 09:52:18 +0800

    [YYB][RTOS][TOOL] Fix tool bug.
    Symptom: Wrong path bug.
    Root Cause: N/A
    Solution: N/A
    Project: YYB.
    Note: N/A
    Test done by RD
       -Build pass: Y
       -Feature verification: Y

--- rtos/cmd_bat/init_zip.cmd
+++ rtos/cmd_bat/init_zip.cmd
@@ -6,18 +6,25 @@ set nrfutilPath="C:\Program Files\Nordic Semiconductor\Master Control Panel\3.10
 ::set projectRootDir="Y:\yyb\hw\nRF51822_Nordic\nRF51822-BK-FW\Source Code\"
 set projectRootDir=Y:\yyb\rtos\nrf51822\beacon\
 
+set CUR_DIR=%cd%
+echo %CUR_DIR%
+	
 IF %1==app (
 	::default update applicaton
 	set outputFileName=nrf51822_beacon_app
 	set outputPath=%projectRootDir%nrf51_beacon\ble_app_beacon\pca20006\s110\arm\_build
-	set CUR_DIR=%~dp0
+
 	cd %projectRootDir%nrf51_beacon\ble_app_beacon\pca20006\s110\arm\
 	call nrf51822_xxac_s110.BAT
-	::cd CUR_DIR
+	cd %CUR_DIR%
 )
 IF %1==bl (
 	set outputFileName=nrf51822_beacon_bootloader
 	set outputPath=%projectRootDir%nrf51_beacon\ble_app_beacon_dfu\bootloader\pca20006\dual_bank_ble_s110\arm\_build
+	
+	cd %projectRootDir%nrf51_beacon\ble_app_beacon_dfu\bootloader\pca20006\dual_bank_ble_s110\arm\
+	call nrf51822_xxac.BAT
+	cd %CUR_DIR%
 )
 set axfFileName=%outputFileName%.axf
 set binFileName=%outputFileName%.bin
